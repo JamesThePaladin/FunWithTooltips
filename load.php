e205d6348574b75b30e253d63742cdcd98b48622
James Pope <jamesthepaladin@gmail.com>
2021-09-14 15:32:43 -0700

    Holiday Update
    Added background events as "holidays" (its the same source I use for the regular events) as an example for functionality. The php files are here just to feel included i guess.

---
+++ load.php
@@ -0,0 +1,34 @@
+<?php
+//load.php
+header("Content-Type: application/json");
+$servername = "localhost";
+$dbname = "test";
+$username = "root";
+$password = "";
+
+$connect = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+$data = array();
+
+$query = "SELECT * FROM events ORDER BY id";
+
+$statement = $connect->prepare($query);
+
+$statement->execute();
+
+$result = $statement->fetchAll();
+
+foreach($result as $row)
+{
+ $data[] = array(
+  'id'   => $row["id"],
+  'title'   => $row["title"],
+  'start'   => $row["start_event"],
+  'end'   => $row["end_event"]
+ );
+}
+
+Header('Content-Type: application/json');
+echo json_encode($data);
+
+?>
